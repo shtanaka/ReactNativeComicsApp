fdcd3ada1ee64d1e902db72c17a84a0f906b257e
Gustavo Santiago <egustavo4@gmail.com>
2019-08-19 01:37:52 -0300

    Add absolute paths to welcome screen

--- app/screens/welcome-screen/welcome-screen.tsx
+++ app/screens/welcome-screen/welcome-screen.tsx
@@ -1,10 +1,10 @@
 import * as React from "react"
 import { View, Image, ViewStyle, TextStyle, ImageStyle, SafeAreaView } from "react-native"
 import { NavigationScreenProps } from "react-navigation"
-import { Text } from "../../components/text"
-import { Button } from "../../components/button"
-import { Screen } from "../../components/screen"
-import { color, spacing } from "../../theme"
+import { Text } from "app/components/text"
+import { Button } from "app/components/button"
+import { Screen } from "app/components/screen"
+import { color, spacing } from "app/theme"
 import { marvelLogo } from "./"
 
 const mainViewStyle: ViewStyle = { flex: 1 }
@@ -55,19 +55,16 @@ export class WelcomeScreen extends React.Component<WelcomeScreenProps, {}> {
   render() {
     return (
       <View style={mainViewStyle}>
-        {/* <Header headerTx="welcomeScreen.poweredBy" style={headerStyle} titleStyle={headerTitleStyle} /> */}
         <Screen style={screenStyle} statusBar="dark-content" preset="scroll">
           <Text style={titleWrapperStyle}>
-            <Text style={titleStyle} text="All your favorite hero comics in one place" />
+            <Text tx="welcomeScreen.title" style={titleStyle} />
           </Text>
           <Image source={marvelLogo} style={logoStyle} />
-          <Text style={descriptionStyle}>
-            This is a Marvel comics listing App. Hope you can find your comics here.
-          </Text>
+          <Text tx="welcomeScreen.description" style={descriptionStyle} />
         </Screen>
         <SafeAreaView style={footerStyle}>
           <View style={footerContentStyle}>
-            <Button tx="welcomeScreen.goToHeroName" onPress={this.goToHeroNameScreen} />
+            <Button tx="welcomeScreen.continue" onPress={this.goToHeroNameScreen} />
           </View>
         </SafeAreaView>
       </View>
